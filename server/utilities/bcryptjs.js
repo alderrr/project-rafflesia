4e601cc1edf93b2c7cb30bcc8c22e69df02e3e4a
alderrr <alifdermayudha@gmail.com>
2024-02-06 16:35:46 +0700

    feat: finished config, bcryptjs, jwt

---
+++ server/utilities/bcryptjs.js
@@ -0,0 +1,12 @@
+const { hashSync, genSaltSync, compareSync } = require("bcryptjs");
+
+function hashPassword(password) {
+  let salt = genSaltSync(10);
+  let hashedPassword = hashSync(password, salt);
+  return hashedPassword;
+}
+function comparePassword(password, hashedPassword) {
+  return compareSync(password, hashedPassword);
+}
+
+module.exports = { hashPassword, comparePassword };
